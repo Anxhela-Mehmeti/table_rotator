c1dd29e02e4e190b0ba6ba13adb328c436be9015
Anxhela Mehmeti <anxhela.mehmeti@e-farm.com>
2023-05-09 23:33:25 +0200

    add the array manipulation methods

--- src/helper.ts
+++ src/helper.ts
@@ -1,15 +1,58 @@
-export const convertArrayTo2D = (table: Number[]) => {
+export const converLinearArrayTo2D = (table: number[]): number[][] => {
   const length = Math.sqrt(table.length);
   if (length % 1 != 0) {
     throw new Error(
       "The table cannot have the same number of columns and rows."
     );
   }
-  const table2d: Number[][] = [];
+  const table2d: number[][] = [];
 
   for (let i = 0; i < length; i++) {
     table2d.push(table.splice(0, length));
   }
 
   return table2d;
+};
+
+export const rotate2DTableLeft = (inputTable: number[][]): number[][] => {
+  const length = inputTable.length;
+
+  const outputTable: (number | null)[][] = Array.from({ length: length }, () =>
+    Array.from({ length: length }, () => null)
+  );
+
+  for (let x = 0; x < length; x++) {
+    for (let y = 0; y < length; y++) {
+      let left = Boolean(x < length - 1 && y <= x && y <= length - x - 2);
+      let bottom = Boolean(x >= length / 2 && y < x && y >= length - x - 1);
+      let right = Boolean(
+        y >= length / 2 && x > 0 && y >= x && y > length - x - 1
+      );
+      let top = Boolean(x < length / 2 && y > 0 && y > x && y < length - x);
+
+      if (left) {
+        outputTable[x + 1][y] = inputTable[x][y];
+      } else if (bottom) {
+        outputTable[x][y + 1] = inputTable[x][y];
+      } else if (right) {
+        outputTable[x - 1][y] = inputTable[x][y];
+      } else if (top) {
+        outputTable[x][y - 1] = inputTable[x][y];
+      } else {
+        outputTable[x][y] = inputTable[x][y];
+      }
+    }
+  }
+
+  return outputTable;
+};
+
+export const convert2DArrayToLinear = (table: number[][]): number[] => {
+  return [].concat.apply([], table);
+};
+
+export const rotateLeftLinearArrayAndConvertTo2D = (inputArray: number[]) => {
+  const table = converLinearArrayTo2D(inputArray);
+  const rotatedTable = rotate2DTableLeft(table);
+  const rotatedLinearArray = convert2DArrayToLinear(rotatedTable);
 };
--- test/helper.test.ts
+++ test/helper.test.ts
@@ -1,4 +1,8 @@
-import { convertArrayTo2D } from "../src/helper";
+import {
+  converLinearArrayTo2D,
+  convert2DArrayToLinear,
+  rotate2DTableLeft,
+} from "../src/helper";
 
 describe("convertArrayto2D", () => {
   it("Should return a 2D array", () => {
@@ -13,15 +17,15 @@ describe("convertArrayto2D", () => {
       [4, 5, 6],
       [7, 8, 9],
     ];
-    expect(convertArrayTo2D(inputData)).toEqual(expectedResult);
-    expect(convertArrayTo2D(inputData2)).toEqual(expectedResult2);
+    expect(converLinearArrayTo2D(inputData)).toEqual(expectedResult);
+    expect(converLinearArrayTo2D(inputData2)).toEqual(expectedResult2);
   });
   it("Should throw if the table cannot have same number of cols and rows", () => {
     const inputData = [1, 2, 3];
     let error;
 
     try {
-      convertArrayTo2D(inputData);
+      converLinearArrayTo2D(inputData);
     } catch (err) {
       error = err.message;
     }
@@ -29,5 +33,59 @@ describe("convertArrayto2D", () => {
     expect(error).toBe(
       "The table cannot have the same number of columns and rows."
     );
+  });
+});
+
+describe("convert2DArrayToLinear", () => {
+  it("Should return one dimensional array", () => {
+    const inputData = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+    ];
+    const expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    expect(convert2DArrayToLinear(inputData)).toEqual(expectedResult);
+  });
+});
+
+describe("rotate2DTableLeft", () => {
+  it("Should rotate the table 1 cell to the left", () => {
+    const inputData3 = [
+      [1, 2, 3, 4, 5],
+      [6, 7, 8, 9, 10],
+      [11, 12, 13, 14, 15],
+      [16, 17, 18, 19, 20],
+      [21, 22, 23, 24, 25],
+    ];
+    const expectedResult3 = [
+      [2, 3, 4, 5, 10],
+      [1, 8, 9, 14, 15],
+      [6, 7, 13, 19, 20],
+      [11, 12, 17, 18, 25],
+      [16, 21, 22, 23, 24],
+    ];
+    const inputData2 = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+    ];
+    const expectedResult2 = [
+      [2, 3, 6],
+      [1, 5, 9],
+      [4, 7, 8],
+    ];
+    const inputData1 = [
+      [1, 2],
+      [4, 5],
+    ];
+    const expectedResult1 = [
+      [2, 5],
+      [1, 4],
+    ];
+
+    expect(rotate2DTableLeft(inputData2)).toEqual(expectedResult2);
+    expect(rotate2DTableLeft(inputData1)).toEqual(expectedResult1);
+    expect(rotate2DTableLeft(inputData3)).toEqual(expectedResult3);
   });
 });
